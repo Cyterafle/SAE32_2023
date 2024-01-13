645020f7ae833d8277534de95125c5dfd7f1325e
thuret <thuret.come@gmail.com>
2024-01-13 15:23:09 +0100

    JavaDoc CellListener

--- src/fr/iutfbleau/chauveau/ngwalang/thuret/excel/CellListener.java
+++ src/fr/iutfbleau/chauveau/ngwalang/thuret/excel/CellListener.java
@@ -7,15 +7,29 @@ public class CellListener extends MouseAdapter {
         private static ModelTableur model;
         private static VueTableur vue;
 
+        /**
+        * Classe <code>CellListener</code>
+        * Elle sert la création d'un objet de type CellListener et stocke les coordonnés d'une cellule.
+        * @param ligne , int représentant la ligne sur laquel est la cellule.
+        * @param colonne , int représentant la colonne sur laquel est la cellule. 
+        */
         public CellListener(int ligne, int colonne) {
             this.ligne = ligne;
             this.colonne = colonne;
         }
 
+        /**
+        * Permet de mettre à jour les données du modèle dans la cellule.
+        * @param m , objet de type ModelTableur représentant la version du modèle à jour.
+        */
         public static void setModel(ModelTableur m){
             model = m;
         }
 
+        /**
+        * Permet de mettre à jour les données de la vue dans la cellule.
+        * @param v , objet de type VueTableur représentant la version de la vue à jour.
+        */
         public static void setVue(VueTableur v){
             vue = v;
         }
